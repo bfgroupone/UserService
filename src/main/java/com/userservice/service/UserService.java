c390c03780d9709c938cb2bcc77ac0ce7f3956bc
Peiqi Chen <chenpeiqi9915@gmail.com>
2023-07-15 00:16:28 -0400

    first commit

---
+++ src/main/java/com/userservice/service/UserService.java
@@ -0,0 +1,12 @@
+package com.userservice.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    public String getAllUsers(){
+        return "USER!";
+    }
+}
