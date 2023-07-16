5483e0ab90bc72bb7151febc0902084f3c70545a
Peiqi Chen <chenpeiqi9915@gmail.com>
2023-07-15 21:38:42 -0400

    fix application.properties

--- src/main/java/groupone/userservice/entity/User.java
+++ src/main/java/groupone/userservice/entity/User.java
@@ -1,2 +1,54 @@
-package groupone.userservice.entity;public class User {
+package groupone.userservice.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name="user")
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userId", unique = true)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @JsonProperty
+    private boolean active;
+
+    @Column(nullable = false)
+    private Date dateJoined;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column
+    private String profileImageURL;
 }
